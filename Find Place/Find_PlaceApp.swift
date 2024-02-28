9a416105aff12c8944e122f73db1d37ac1d43664
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2024-02-28 01:36:38 +0100

    Initial Commit

---
+++ Find Place/Find_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_PlaceApp.swift
+//  Find Place
+//
+//  Created by Victor Ezea on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Find_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
